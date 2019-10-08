f58f8e065ead05da56d472b472009387c2513fb1
jdafer98 <jdafer98@gmail.com>
2019-10-08 11:27:14 +0200

    ultimos ajustes en fabfile working on #4

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,3 @@
-#Autor: Javier de Ángeles Fernández 2019
-#Ordenes: 
-#	fab build --> Instala todos los requisitos para que la aplicación pueda funcionar.
-#	fab test --> Pasa los test correspondientes para comprobar la integridad del código.
-
-#NOTA: Es posible que sea necesario cambiar la orden "pip install -r requirements.txt" y añadir
-# "pip install --user -r requirements.txt" en caso de tener algún problema relacionado con
-# permisos.
 
 from fabric import task
 from invoke import run
