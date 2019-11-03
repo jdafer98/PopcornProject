322c8e5b9fdb0df0cf28a7f2d604f6eae3c55c4d
jdafer98 <jdafer98@gmail.com>
2019-11-03 16:55:36 +0100

    añadido nuevos task #17

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,19 @@ from invoke import run
 
 @task
 def build(name):
+	run("export CV3_PORT=31416")
 	run("pip install -r requirements.txt")
-
+	run("sudo mv ./controv3rsial.conf /etc/supervisor/conf.d")
 @task
 def test(name):
 	run("pytest")
-
-
-
-	
+@task
+def start(name):
+	print("Recuerda especificar un puerto en la variable de entorno mediante 'export CV3_PORT=<num_puerto>'")
+	run("sudo supervisorctl reread && sudo supervisorctl reload")
+@task
+def stop(name):
+	run("sudo supervisorctl stop cv3")
+@task
+def status(name):
+	run("sudo supervisorctl status")
