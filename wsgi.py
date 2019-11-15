35485e27d8dc589f746285bfdd15dc70d2b31084
jdafer98 <jdafer98@gmail.com>
2019-11-15 21:31:36 +0100

    Arreglando mis test de Travis #22

--- wsgi.py
+++ wsgi.py
@@ -3,45 +3,45 @@ from encuestas import *
 import os
 
 
-application = Flask(__name__)
+appliaction = Flask(__name__)
 enc = Encuestas()
 
-@application.route('/')
+@appliaction.route('/')
 def raiz():
 	return jsonify(status='OK')
 
-@application.route('/status')
+@appliaction.route('/status')
 def status():
 	return jsonify(status='OK')
 
-@application.route('/crear_encuesta',methods=['POST'])
+@appliaction.route('/crear_encuesta',methods=['POST'])
 def ruta_crear_encuesta():
 	nueva_encuesta = enc.crear_encuesta(request.form['name'])
 	enc.add(nueva_encuesta)
 	return jsonify(enc.encuesta2json(nueva_encuesta))
 
-@application.route('/votar', methods=['PUT'])
+@appliaction.route('/votar', methods=['PUT'])
 def ruta_votar():
 	a = enc.extraer_por_hashcode(request.form['hc'])
 	if a != -1:
 		if int(request.form['opcion']) == 1:
 			enc.votar(a,'O1')
-			return jsonify(status='200 OK')
+			return jsonify(status='OK')
 		elif int(request.form['opcion']) == 2:
 			enc.votar(a,'O2')
-			return jsonify(status='200 OK')
+			return jsonify(status='OK')
 		elif int(request.form['opcion']) == 3:
 			enc.votar(a,'O3')
-			return jsonify(status='200 OK')
+			return jsonify(status='OK')
 		elif int(request.form['opcion']) == 4:
 			enc.votar(a,'O4')
-			return jsonify(status='200 OK')
+			return jsonify(status='OK')
 		else:
 			return jsonify(error='400 INVALID OPTION'), 400
 	else:
 		return jsonify(error='404 NOT FOUND'), 404
 
-@application.route('/get_encuesta',methods=['GET'])
+@appliaction.route('/get_encuesta',methods=['GET'])
 def get_encuesta():
 	a = enc.extraer_por_hashcode(request.args.get('hc'))
 	if a != -1:
@@ -49,11 +49,11 @@ def get_encuesta():
 	else:
 		return jsonify(error='404 NOT FOUND'), 404
 
-@application.route('/eliminar_encuesta',methods=['DELETE'])
+@appliaction.route('/eliminar_encuesta',methods=['DELETE'])
 def eliminar_encuesta():
 	a = enc.eliminar_por_hashcode(request.form['hc'])
 	if a == 0:
-		return jsonify(status='200 OK')
+		return jsonify(status='OK')
 	else:
 		return jsonify(error='404 NOT FOUND'), 404
 
